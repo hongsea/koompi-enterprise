94e7b3e7a63eae51705741dcd581995e726915fa
IsaacJReay <isaacjacksonreay@gmail.com>
2020-11-09 16:56:28 +0700

    fix link to sambafile

--- setup.sh
+++ setup.sh
@@ -72,19 +72,21 @@ function sambainput(){
     --backtitle "Samba Active Directory Domain Controller" \
     --nocancel \
     --ok-button Submit \
-    --inputbox "\nPlease enter a realm name for the active directory server.\n\nExample:  KOOMPILAB.ORG\n" \
+    --inputbox "\nPlease enter a realm name for the active directory server.\n\nExample:  INTERNAL.KOOMPILAB.ORG\n" \
     10 80 3>&1 1>&2 2>&3)
     
-    secondlvl_domain=$(echo $samba_realm |awk -F'.' '{printf $NF}')
-    samba_domain=${samba_realm//".$secondlvl_domain"}
+    secondlvl_domain=$(echo $samba_realm |awk -F'.' '{printf $NF}') #ORG
+    samba_domain=${samba_realm//".$secondlvl_domain"} #INTERNAL.KOOMPILAB.ORG-.ORG = INTERNAL.KOOMPILAB
+
+    full_domain=$samba_domain #INTERNAL.KOOMPILAB
 
     if [[ "$samba_domain" == *.* ]];
     then
-        samba_domain=$(echo "$samba_domain" | awk -F'.' '{printf $1}')
+        samba_domain=$(echo "$samba_domain" | awk -F'.' '{printf $1}') #INTERNAL.KOOMPILAB-.KOOMPILAB = INTERNAL
     fi
 
-    samba_realm=${samba_realm^^}
-    samba_domain=${samba_domain^^}
+    samba_realm=${samba_realm^^} #INTERNAL.KOOMPILAB.ORG
+    samba_domain=${samba_domain^^} #INTERNAL
 
 
     while true;
@@ -435,7 +437,8 @@ function samba(){
 
     grep -rli HOSTNAME $SMB | xargs -i@ sed -i s+HOSTNAME+$HOSTNAME+g @
     grep -rli REALM $SMB | xargs -i@ sed -i s+REALM+$samba_realm+g @
-    grep -rli DOMAIN $SMB | xargs -i@ sed -i s+DOMAIN+$samba_domain+g @
+    # grep -rli LONG_DOMAIN $SMB | xargs -i@ sed -i s+DOMAIN+$samba_domain+g @
+    grep -rli LONG_DOMAIN $SMB | xargs -i@ sed -i s+DOMAIN+$full_domain+g @
     # echo -e "# Global parameters" > $SMB
     # echo -e "[global]" >> $SMB
     # echo -e "\tnetbios name = $HOSTNAME" >> $SMB
