b43312d6e49ce3f30665becfafac5b4e2f449236
IsaacJReay <isaacjacksonreay@gmail.com>
2021-01-26 16:53:11 +0700

    readd env

--- setup.sh
+++ setup.sh
@@ -253,9 +253,7 @@ function inputcheck(){
         fi
     done
 
-    echo -e "\nSAMBAPROFILE=$PROFILESPATH\nSAMBAHOME=$HOMEPATH\nSAMBANETLOGON=$NETLOGONPATH\n" >> ~/.bashrc
-
-    source ~/.bashrc
+    echo -e "\nSAMBAPROFILE=$PROFILESPATH\nSAMBAHOME=$HOMEPATH\nSAMBANETLOGON=$NETLOGONPATH\n" | sudo tee -a /etc/environment
     
 }
 
